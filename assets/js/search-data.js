2b47982e301a5b7512474833a2ae720133e6fd9c
palomapiot <paloma.piot@udc.es>
2026-02-09 11:01:16 +0000

    Deploying to gh-pages from @ palomapiot/palomapiot.github.io@8a76aef9164c0ad8eed51240741d4ab162312cf5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -385,12 +385,7 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-na",
-          title: 'NA',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-new-site",
+      },{id: "news-new-site",
           title: 'New site!',
           description: "",
           section: "News",},{id: "projects-hybrids",
